a11ca005766d647347ad6333f00afccffddcee37
pierrialexandervidmar <pierri@hotmail.com.br>
2025-05-11 20:30:38 -0300

    fix: logger

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"github.com/pierrialexandervidmar/go-jobs/config"
 	"github.com/pierrialexandervidmar/go-jobs/router"
+)
+
+var (
+	logger *config.Logger
 )
 
 func main() {
 
-	// Inicialização do Router
+	logger = config.GetLogger("main")
+
+	// Initialize config
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 
 }
